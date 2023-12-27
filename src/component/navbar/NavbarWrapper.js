951901536a19e187c0678fba4c1b04a5ed96a1c0
dgett <diego.gettatelli@outlook.it>
2023-12-27 22:35:38 +0100

    feat(navbar): Added Logo

--- src/component/navbar/NavbarWrapper.js
+++ src/component/navbar/NavbarWrapper.js
@@ -3,12 +3,16 @@ import HomeIcon from '@rsuite/icons/legacy/Home';
 import CogIcon from '@rsuite/icons/legacy/Cog';
 import { IconName } from "react-icons/io5";
 import { IoLogOut } from "react-icons/io5";
+import logo from '../../resources/navbar_logo_nobg.png';
+import './NavbarWrapper.css';
 
 function NavbarWrapper() {
     return (
         <div>
             <Navbar>
-                <Navbar.Brand href="#">CHRONOS</Navbar.Brand>
+                <Navbar.Brand>
+                    <img src={logo} alt="logo" className="logo" />
+                </Navbar.Brand>
                 <Nav>
                     <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
                     <Nav.Menu title="About">
