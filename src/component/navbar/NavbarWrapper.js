34537a833c1b0732ef6cda2df752c0eca1ce04e0
dgett <diego.gettatelli@outlook.it>
2023-12-27 22:12:31 +0100

    feat(navbar): Changed menu item and installed react-icons

--- src/component/navbar/NavbarWrapper.js
+++ src/component/navbar/NavbarWrapper.js
@@ -1,6 +1,8 @@
 import { Navbar, Nav } from 'rsuite';
 import HomeIcon from '@rsuite/icons/legacy/Home';
 import CogIcon from '@rsuite/icons/legacy/Cog';
+import { IconName } from "react-icons/io5";
+import { IoLogOut } from "react-icons/io5";
 
 function NavbarWrapper() {
     return (
@@ -9,19 +11,13 @@ function NavbarWrapper() {
                 <Navbar.Brand href="#">CHRONOS</Navbar.Brand>
                 <Nav>
                     <Nav.Item icon={<HomeIcon />}>Home</Nav.Item>
-                    <Nav.Item>News</Nav.Item>
-                    <Nav.Item>Products</Nav.Item>
                     <Nav.Menu title="About">
                         <Nav.Item>Company</Nav.Item>
-                        <Nav.Item>Team</Nav.Item>
-                        <Nav.Menu title="Contact">
-                            <Nav.Item>Via email</Nav.Item>
-                            <Nav.Item>Via telephone</Nav.Item>
-                        </Nav.Menu>
                     </Nav.Menu>
                 </Nav>
                 <Nav pullRight>
                     <Nav.Item icon={<CogIcon />}>Settings</Nav.Item>
+                    <Nav.Item icon={<IoLogOut />}>Logout</Nav.Item>
                 </Nav>
             </Navbar>
         </div>
