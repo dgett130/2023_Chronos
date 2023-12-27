a36862bbeb1f5002763828b6745651edbb5caeda
dgett <diego.gettatelli@outlook.it>
2023-12-27 22:39:41 +0100

    feat(sidebar): Changed Item

--- src/component/sidebar/Sidebar.js
+++ src/component/sidebar/Sidebar.js
@@ -11,31 +11,24 @@ function Sidebar(props) {
 
     return (
         <div>
-            <Sidenav expanded={props.expanded} defaultOpenKeys={['3', '4']}>
+            <Sidenav expanded={props.expanded} defaultOpenKeys={[]}>
                 <Sidenav.Toggle expanded={props.expanded} onToggle={onSidebarToggle} />
                 <Sidenav.Body>
                     <Nav activeKey={activeKey} onSelect={setActiveKey}>
                         <Nav.Item eventKey="1">
                             Dashboard
                         </Nav.Item>
-                        <Nav.Menu placement="rightStart" eventKey="3" title="Advanced" >
-                            <Nav.Item eventKey="3-1">Geo</Nav.Item>
-                            <Nav.Item eventKey="3-2">Devices</Nav.Item>
-                            <Nav.Item eventKey="3-3">Loyalty</Nav.Item>
-                            <Nav.Item eventKey="3-4">Visit Depth</Nav.Item>
+                        <Nav.Menu placement="rightStart" eventKey="3" title="Avanzate" >
+                            {/*<Nav.Item eventKey="3-1">Geo</Nav.Item>*/}
+                        </Nav.Menu>
+                        <Nav.Menu placement="rightStart" eventKey="5" title="Profilo" >
                         </Nav.Menu>
                         <Nav.Menu
                             placement="rightStart"
                             eventKey="4"
-                            title="Settings"
+                            title="Impostazioni"
                         >
-                            <Nav.Item eventKey="4-1">Applications</Nav.Item>
-                            <Nav.Item eventKey="4-2">Channels</Nav.Item>
-                            <Nav.Item eventKey="4-3">Versions</Nav.Item>
-                            <Nav.Menu eventKey="4-5" title="Custom Action">
-                                <Nav.Item eventKey="4-5-1">Action Name</Nav.Item>
-                                <Nav.Item eventKey="4-5-2">Action Params</Nav.Item>
-                            </Nav.Menu>
+                            <Nav.Item eventKey="4-1">Applicazione</Nav.Item>
                         </Nav.Menu>
                     </Nav>
                 </Sidenav.Body>
