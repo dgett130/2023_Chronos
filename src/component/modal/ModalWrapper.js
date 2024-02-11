e531b59d828d05799285ea3dfdb0fb95a25f8118
dgett <diego.gettatelli@outlook.it>
2024-02-11 23:28:25 +0100

    feat(loading): Added loading simulation

--- src/component/modal/ModalWrapper.js
+++ src/component/modal/ModalWrapper.js
@@ -1,5 +1,5 @@
 import {React, useEffect, useState} from 'react';
-import {Modal, ButtonToolbar, Button, RadioGroup, Radio, Placeholder, Cascader, Slider} from 'rsuite';
+import {Modal, ButtonToolbar, Button, RadioGroup, Radio, Placeholder, Cascader, Slider, Loader} from 'rsuite';
 import {mockedDataForJobsCascader} from '../mockData.js';
 import './ModalWrapper.css';
 import { projectAPI } from '../apis/projectAPI.js';
@@ -25,6 +25,7 @@ function ModalWrapper({isOpen, onClose, selectedDate}) {
     const [projectsList, setProjectsList] = useState([]);
     const [project_types, setProject_types] = useState([]);
     const [projectHours, setProjectHours] = useState(0);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         projectAPI.getAll().then((response) => {
@@ -39,39 +40,61 @@ function ModalWrapper({isOpen, onClose, selectedDate}) {
         });
     },[]);
 
+    const handleOreChange = (value, event) => {
+        setProjectHours(value);
+    }
+
+    const handleProjectChange = (value, event) => {
+        setProjectHours(0);
+    }
+
+    const handleSave = () => {
+        setLoading(true);
+        console.log("Salvataggio in corso...");
+        setTimeout(() => {
+            handleClose();
+            setLoading(false);
+        }, 2000);
+    }
+
     return (
-        <Modal backdrop={backdrop} keyboard={false} open={isOpen} onClose={handleClose}>
-            <Modal.Header>
-                <Modal.Title>Attività</Modal.Title>
-            </Modal.Header>
+        <div>
+            <Modal backdrop={backdrop} keyboard={false} open={isOpen} onClose={handleClose}>
+                <Modal.Header>
+                    <Modal.Title>Attività</Modal.Title>
+                </Modal.Header>
 
-            <Modal.Body>
-                <h3>{selectedDate?.toDateString()}</h3>
-                <br/>
-                {/*<Placeholder.Graph/>*/}
-                <div>
-                    <h4>Progetto</h4>
-                    <Cascader data={mapModalProjectSelection(projectsList, project_types)} placeholder="Seleziona un progetto"/>
-                </div>
-                <div>
-                    <h4>Ore</h4>
-                    <InputNumber defaultValue={0} min={0} max={8} step={0.50} onChange={handleOreChange}/>
-                </div>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button onClick={handleClose} appearance="primary">
-                    Salva
-                </Button>
-                <Button onClick={handleClose} appearance="subtle">
-                    Chiudi
-                </Button>
-            </Modal.Footer>
-        </Modal>
+                <Modal.Body>
+                    <div>
+                        {loading && <div>
+                            <Loader backdrop size="md" content="Salvataggio..." vertical/>
+                        </div>}
+                        <h3>{selectedDate?.toDateString()}</h3>
+                        <br/>
+                        {/*<Placeholder.Graph/>*/}
+                        <div>
+                            <h4>Progetto</h4>
+                            <Cascader data={mapModalProjectSelection(projectsList, project_types)}
+                                      onChange={handleProjectChange}
+                                      placeholder="Seleziona un progetto"/>
+                        </div>
+                        <div>
+                            <h4>Ore</h4>
+                            <InputNumber value={projectHours} defaultValue={0} min={0} max={8} step={0.50} onChange={handleOreChange}/>
+                        </div>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleSave} appearance="primary">
+                        Salva
+                    </Button>
+                    <Button onClick={handleClose} appearance="subtle">
+                        Chiudi
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     );
-}
-
-const handleOreChange = (value, event) => {
-    setProjectHours(value);
 }
 
 export default ModalWrapper;
